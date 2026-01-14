3e4a0f827fe058ecf6db3a5171ab8374bd548f2e
koyonn <melisneadekvat@gmail.com>
2026-01-14 18:21:29 +0300

    Test commit

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -112,6 +112,7 @@ export default async function decorate(block) {
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
   const fragment = await loadFragment(navPath);
+  console.log('navPath: ' + navPath);
 
   // decorate nav DOM
   block.textContent = '';
